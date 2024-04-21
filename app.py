8a10feab96d60b665f77c0a9191f8ffa66708ce4
manan-bedi2908 <mananbedilps@gmail.com>
2024-04-21 16:39:35 +0530

    added prompt

--- app.py
+++ app.py
@@ -7,3 +7,30 @@ import google.generativeai as genai
 load_dotenv()
 
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+def get_gemini_response(question, prompt):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content([prompt, question])
+    return response.text
+
+def read_sql_query(sql, db):
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    return rows
+
+prompt = [
+    """
+    You are an expert in converting English questions to SQL queries.
+    The SQL database has the name STUDENT and has the following columns:
+    NAME, CLASS, SECTION \n\nFor example, \nExample 1 - How 
+    many entries of records are present?,
+    the SQL command will be something like this 
+    SELECT COUNT(*) from STUDENT;
+    also the SQL query should not have ''' in beginning or end 
+    and sql word in the output.
+    """
+]
